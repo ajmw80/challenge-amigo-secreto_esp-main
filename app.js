0e3d19e36f5f24bbe5a9197e81af2e35e8c2ca8f
anrryd.moran <anrryd.moran@outlook.com>
2025-08-13 23:18:00 -0500

    Agregar nombres

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-let amigos = [];
+let listaAmigos = [];
+
+const inputAmigo = document.getElementById("amigo");
+const listaAmigoUl = document.getElementById("listaAmigos");
+
+function agregarAmigo(){
+    listaAmigos.push(inputAmigo.value);
+    listaAmigoUl.innerHTML += `<li>${inputAmigo.value}</li>`;
+}
 
 
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function sortearAmigo(){
+    const random = Math.floor(Math.random()* listaAmigos.length);
+    const amigoSecreto = listaAmigos[random];
+    listaAmigoUl.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+}
+
+
